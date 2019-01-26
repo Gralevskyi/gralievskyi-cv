749b7db3128ca1d2afc59c7be968652c02516772
Alexandr Gralevskyi <ogralievskyi@gmail.com>
2019-01-26 17:09:54 +0000

    make it better

---
+++ src/main/java/portfolio/MvcConf.java
@@ -0,0 +1,23 @@
+package portfolio;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConf implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("/img/**")
+                .addResourceLocations("classpath:/img/");
+        registry.addResourceHandler("/js/**")
+                .addResourceLocations("classpath:/js/");
+        registry.addResourceHandler("/webfonts/**")
+                .addResourceLocations("classpath:/webfonts/");
+    }
+
+}
---
+++ src/main/java/portfolio/PortfolioController.java
@@ -0,0 +1,16 @@
+package portfolio;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class PortfolioController {
+
+    @GetMapping("/")
+    public String portfolio(Model model) {
+        return "index";
+    }
+
+}
